aba8ad24b4f92fc1f82957bd1da81c91e6629fbd
Ansh <anshkhantwal04@gmail.com>
2026-07-07 15:56:10 +0530

    Map Fixed Footer and Network Section

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -43,11 +43,11 @@ export class FooterComponent {
   ];
 
   networkOfCare2: LinkItem[] = [
-    { name: 'Lawrence Road', url: 'https://www.google.com/maps/embed?pb=!4v1783416717613!6m8!1m7!1sBhRGKNnLjK77aOi7f4HvJw!2m2!1d28.68975231215174!2d77.16094850828438!3f208.77!4f-31.759999999999998!5f1.0228038414546936' },
-    { name: 'Mayur Vihar', url: 'https://www.google.com/maps/embed?pb=!4v1783416775372!6m8!1m7!1sV1PyN3wAw9Ksw1byVG7xZQ!2m2!1d28.60691070696357!2d77.29596978858156!3f252.51!4f6.799999999999997!5f0.8134586354752991' },
+    { name: 'Lawrence Road', url: 'https://www.google.com/maps?q=28.68975231215174,77.16094850828438' },
+    { name: 'Mayur Vihar', url: 'https://www.google.com/maps?q=28.60691070696357,77.29596978858156' },
     { name: 'Durgapuri', url: 'https://www.google.com/maps/place/MED+CROSS+CLINIC/@28.690701,77.290205,15z/data=!4m6!3m5!1s0x390cfb5f78cc7cff:0xbe4a1cd7f063d6e!8m2!3d28.6907012!4d77.290205!16s%2Fg%2F11v6fkpgc8?hl=en&entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D' },
-    { name: 'Uttam Nagar', url: 'https://www.google.com/maps/embed?pb=!4v1783416910122!6m8!1m7!1sliYdcGvYO6kGm4_agadMGg!2m2!1d28.62084153104128!2d77.04939339192077!3f358.29!4f-0.12000000000000455!5f0.7820865974627469' },
-    { name: 'Tigri  ', url: 'https://www.google.com/maps/embed?pb=!4v1783416622731!6m8!1m7!1sV_v1Sou5sdWc-VAGIwRhvg!2m2!1d28.51170377389819!2d77.23801085778919!3f269.02!4f-3.0799999999999983!5f0.7820865974627469' },
+    { name: 'Uttam Nagar', url: 'https://www.google.com/maps?q=28.62084153104128,77.04939339192077' },
+    { name: 'Tigri  ', url: 'https://www.google.com/maps?q=28.51170377389819,77.23801085778919' },
   ];
 
   socialMedia = [
--- src/app/components/network/network.component.ts
+++ src/app/components/network/network.component.ts
@@ -9,7 +9,8 @@ import { UrlEncodePipe } from '../pipes/url-encode.pipe';
 interface Location {
  branchName: string; 
  addressLine: string; 
- mapEmbedUrl: string; 
+ mapEmbedUrl: string;
+ streetViewUrl: string; 
 }
 
 @Component({
@@ -29,27 +30,32 @@ export class NetworkComponent {
   {
    branchName: 'Mayur Vihar Branch',
    addressLine: 'TB Clinic, DDA Market, F-7, Mayur Vihar Phase-I, Pocket-1, Delhi-110091',
-   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416775372!6m8!1m7!1sV1PyN3wAw9Ksw1byVG7xZQ!2m2!1d28.60691070696357!2d77.29596978858156!3f252.51!4f6.799999999999997!5f0.8134586354752991'
+   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416775372!6m8!1m7!1sV1PyN3wAw9Ksw1byVG7xZQ!2m2!1d28.60691070696357!2d77.29596978858156!3f252.51!4f6.799999999999997!5f0.8134586354752991',
+   streetViewUrl: 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=28.60691070696357,77.29596978858156'
   },
   {
    branchName: 'Lawrence Road Branch',
    addressLine: 'C-8/139, Lawrence Road Gate No :3, Keshavpuram, Delhi-35, Opp- B4 Dda Market(Hanuman Mandir)',
-   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416717613!6m8!1m7!1sBhRGKNnLjK77aOi7f4HvJw!2m2!1d28.68975231215174!2d77.16094850828438!3f208.77!4f-31.759999999999998!5f1.0228038414546936'
+   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416717613!6m8!1m7!1sBhRGKNnLjK77aOi7f4HvJw!2m2!1d28.68975231215174!2d77.16094850828438!3f208.77!4f-31.759999999999998!5f1.0228038414546936',
+   streetViewUrl: 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=28.68975231215174,77.16094850828438'
   },
   {
    branchName: 'Durgapuri Branch',
    addressLine: 'C-45, West Jyoti Nagar, Durgapuri Chowk, Durgapuri, Shahdara, Delhi-110094',
-   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.964745626593!2d77.28763007529133!3d28.6907011756331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfb5f78cc7cff%3A0xbe4a1cd7f063d6e!2sMED%20CROSS%20CLINIC!5e0!3m2!1sen!2sin!4v1765006663135!5m2!1sen!2sin'
+   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.964745626593!2d77.28763007529133!3d28.6907011756331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfb5f78cc7cff%3A0xbe4a1cd7f063d6e!2sMED%20CROSS%20CLINIC!5e0!3m2!1sen!2sin!4v1765006663135!5m2!1sen!2sin',
+   streetViewUrl: 'https://www.google.com/maps/search/?api=1&query=C-45+West+Jyoti+Nagar+Durgapuri+Delhi'
   },
   {
    branchName: 'Uttam Nagar Branch',
    addressLine: 'RZ-10, Uttam Nagar, Main Najafgarh Road, Near Pillar No. 713, Next to IDBI Bank, Uttam Nagar',
-   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416910122!6m8!1m7!1sliYdcGvYO6kGm4_agadMGg!2m2!1d28.62084153104128!2d77.04939339192077!3f358.29!4f-0.12000000000000455!5f0.7820865974627469'
+   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416910122!6m8!1m7!1sliYdcGvYO6kGm4_agadMGg!2m2!1d28.62084153104128!2d77.04939339192077!3f358.29!4f-0.12000000000000455!5f0.7820865974627469',
+   streetViewUrl: 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=28.62084153104128,77.04939339192077'
   },
   {
    branchName: 'Tigri Branch',
    addressLine: 'A Block 111, 112, 113 Ground floor Bank Road JJ Colony Tigri, New Delhi-110080(near Sheetla Mata Mandir)',
-   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416622731!6m8!1m7!1sV_v1Sou5sdWc-VAGIwRhvg!2m2!1d28.51170377389819!2d77.23801085778919!3f269.02!4f-3.0799999999999983!5f0.7820865974627469'
+   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!4v1783416622731!6m8!1m7!1sV_v1Sou5sdWc-VAGIwRhvg!2m2!1d28.51170377389819!2d77.23801085778919!3f269.02!4f-3.0799999999999983!5f0.7820865974627469',
+   streetViewUrl: 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=28.51170377389819,77.23801085778919'
   }
  ];
 
